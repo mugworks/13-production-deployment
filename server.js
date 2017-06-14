6ca76c3b1e9b9cc18f8392ac072f44170c57af57
mugworks <greenwood.pdx@gmail.com>
2017-06-14 13:32:01 -0700

    moved env to gitignore added call to server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
-//TODO include our new npm module that reads our .env file when running our server locally
-
+//DONE include our new npm module that reads our .env file when running our server locally
+require('dotenv').config();
 
 const pg = require('pg');
 const fs = require('fs');
@@ -11,45 +11,45 @@ const requestProxy = require('express-request-proxy'); // REVIEW: We've added a 
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = ''; // TODO: now that we are using environment variables, move our conString to our .env file
+const conString = process.env.DATABASE_URL; // DONE: now that we are using environment variables, move our conString to our .env file
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
 // REVIEW: This is a new proxy method which acts as a 'middle man' (middleware) for our request. 
-// TODO: Set your local environment variable for GITHUB_TOKEN so you can still run your app locally. Don't forget to .gitignore the file holding your precious info! 
-function proxyGitHub( request, response ) {
-  console.log( 'Routing GitHub request for', request.params[0] );
-  ( requestProxy({
+// DONE: Set your local environment variable for GITHUB_TOKEN so you can still run your app locally. Don't forget to .gitignore the file holding your precious info! 
+function proxyGitHub(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
     url: `https://api.github.com/${request.params[0]}`,
-    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+    headers: { Authorization: `token ${process.env.GITHUB_TOKEN}` }
   }))(request, response);
 }
 
 // REVIEW: This is a new route that will utilize our middle man proxy.
 app.get('/github/*', proxyGitHub);
 
-app.get('/new', (request, response) => response.sendFile('new.html', {root: './public'}));
-app.get('/admin', (request, response) => response.sendFile('admin.html', {root: './public'}));
+app.get('/new', (request, response) => response.sendFile('new.html', { root: './public' }));
+app.get('/admin', (request, response) => response.sendFile('admin.html', { root: './public' }));
 app.get('/articles', (request, response) => {
   client.query(`
     SELECT * FROM articles
     INNER JOIN authors
       ON articles.author_id=authors.author_id;`
   )
-  .then(result => response.send(result.rows))
-  .catch(console.error);
+    .then(result => response.send(result.rows))
+    .catch(console.error);
 });
 
-app.post('/articles', function(request, response) {
+app.post('/articles', function (request, response) {
   client.query(
     'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING',
     [request.body.author, request.body.authorUrl],
-    function(err) {
+    function (err) {
       if (err) console.error(err)
       queryTwo()
     }
@@ -59,7 +59,7 @@ app.post('/articles', function(request, response) {
     client.query(
       `SELECT author_id FROM authors WHERE author=$1`,
       [request.body.author],
-      function(err, result) {
+      function (err, result) {
         if (err) console.error(err)
         queryThree(result.rows[0].author_id)
       }
@@ -78,7 +78,7 @@ app.post('/articles', function(request, response) {
         request.body.publishedOn,
         request.body.body
       ],
-      function(err) {
+      function (err) {
         if (err) console.error(err);
         response.send('insert complete');
       }
@@ -94,24 +94,24 @@ app.put('/articles/:id', (request, response) => {
     `,
     [request.body.author, request.body.authorUrl, request.body.author_id]
   )
-  .then(() => {
-    client.query(`
+    .then(() => {
+      client.query(`
       UPDATE articles
       SET author_id=$1, title=$2, category=$3, "publishedOn"=$4, body=$5
       WHERE article_id=$6
       `,
-      [
-        request.body.author_id,
-        request.body.title,
-        request.body.category,
-        request.body.publishedOn,
-        request.body.body,
-        request.params.id
-      ]
-    )
-  })
-  .then(() => response.send('Update complete'))
-  .catch(console.error);
+        [
+          request.body.author_id,
+          request.body.title,
+          request.body.category,
+          request.body.publishedOn,
+          request.body.body,
+          request.params.id
+        ]
+      )
+    })
+    .then(() => response.send('Update complete'))
+    .catch(console.error);
 });
 
 app.delete('/articles/:id', (request, response) => {
@@ -119,14 +119,14 @@ app.delete('/articles/:id', (request, response) => {
     `DELETE FROM articles WHERE article_id=$1;`,
     [request.params.id]
   )
-  .then(() => response.send('Delete complete'))
-  .catch(console.error);
+    .then(() => response.send('Delete complete'))
+    .catch(console.error);
 });
 
 app.delete('/articles', (request, response) => {
   client.query('DELETE FROM articles')
-  .then(() => response.send('Delete complete'))
-  .catch(console.error);
+    .then(() => response.send('Delete complete'))
+    .catch(console.error);
 });
 
 loadDB();
@@ -143,31 +143,31 @@ function loadAuthors() {
         'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING',
         [ele.author, ele.authorUrl]
       )
-      .catch(console.error);
+        .catch(console.error);
     })
   })
 }
 
 function loadArticles() {
   client.query('SELECT COUNT(*) FROM articles')
-  .then(result => {
-    if(!parseInt(result.rows[0].count)) {
-      fs.readFile('./public/data/hackerIpsum.json', (err, fd) => {
-        JSON.parse(fd.toString()).forEach(ele => {
-          client.query(`
+    .then(result => {
+      if (!parseInt(result.rows[0].count)) {
+        fs.readFile('./public/data/hackerIpsum.json', (err, fd) => {
+          JSON.parse(fd.toString()).forEach(ele => {
+            client.query(`
             INSERT INTO
             articles(author_id, title, category, "publishedOn", body)
             SELECT author_id, $1, $2, $3, $4
             FROM authors
             WHERE author=$5;
           `,
-            [ele.title, ele.category, ele.publishedOn, ele.body, ele.author]
-          )
-          .catch(console.error);
+              [ele.title, ele.category, ele.publishedOn, ele.body, ele.author]
+            )
+              .catch(console.error);
+          })
         })
-      })
-    }
-  })
+      }
+    })
 }
 
 function loadDB() {
@@ -179,8 +179,8 @@ function loadDB() {
       "authorUrl" VARCHAR (255)
     );`
   )
-  .then(loadAuthors)
-  .catch(console.error);
+    .then(loadAuthors)
+    .catch(console.error);
 
   client.query(`
     CREATE TABLE IF NOT EXISTS
@@ -193,6 +193,6 @@ function loadDB() {
       body TEXT NOT NULL
     );`
   )
-  .then(loadArticles)
-  .catch(console.error);
+    .then(loadArticles)
+    .catch(console.error);
 }
